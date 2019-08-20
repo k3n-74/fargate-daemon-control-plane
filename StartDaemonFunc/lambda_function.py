e221e8aeb510c725e9546b6e65daf22778bbd620
ken-takehara <dev.kentakehara@gmail.com>
2019-08-20 20:44:25 +0900

    task起動の初期実装

--- StartDaemonFunc/lambda_function.py
+++ StartDaemonFunc/lambda_function.py
@@ -1,10 +1,53 @@
 import json
+import boto3
+import time
+# from aws_xray_sdk.core import patch_all
+from botocore.client import ClientError, Config
+import traceback
+
+# patch_all()
+
+run_task_res = None
 
 def lambda_handler(event, context):
     
     print(json.dumps(event))
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+    cluster = 'daemon-daemon-DaemonEcsCluster-OBWV6JAR18NG'
+    taskDefinition = event.get('taskDefinition')
+    launchType='FARGATE'
+    networkConfiguration={
+        'awsvpcConfiguration' : {
+            'subnets' : ['subnet-03ea9b862022ca374','subnet-03ebf3acc84659532'],
+            # 'securityGroups' : [ '' ],
+            'assignPublicIp' : 'ENABLED'
+        },
     }
+    
+    daemon_id = str(time.time())
+    
+    try:
+        ecs = boto3.client('ecs')
+        run_task_res = ecs.run_task(
+                cluster=cluster,
+                taskDefinition=taskDefinition,
+                launchType=launchType,
+                networkConfiguration=networkConfiguration,
+                # tags=[ { 'key' : 'DCP_DAEMON_ID' , 'value' : daemon_id } ]
+            )
+        
+        
+    except ClientError as e:
+        traceback.print_exc()
+        raise e
+    
+    except Exception as e:
+        traceback.print_exc()
+        raise e
+    
+    ret = {
+        'taskArn' : run_task_res.get('tasks')[0].get('taskArn'),
+        'daemon_id' : daemon_id
+    }
+    
+    return ret
