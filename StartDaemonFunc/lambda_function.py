a3879855254f7ec15efbee4c7e79bd91225e6a78
ken-takehara <dev.kentakehara@gmail.com>
2019-09-07 21:30:54 +0900

    タスクの環境変数にdaemon idを追加

--- StartDaemonFunc/lambda_function.py
+++ StartDaemonFunc/lambda_function.py
@@ -13,30 +13,20 @@ def lambda_handler(event, context):
     
     print(json.dumps(event))
     
-    cluster = 'daemon-daemon-DaemonEcsCluster-OBWV6JAR18NG'
-    taskDefinition = event.get('taskDefinition')
-    launchType='FARGATE'
-    networkConfiguration={
-        'awsvpcConfiguration' : {
-            'subnets' : ['subnet-03ea9b862022ca374','subnet-03ebf3acc84659532'],
-            # 'securityGroups' : [ '' ],
-            'assignPublicIp' : 'ENABLED'
-        },
-    }
-    
-    daemon_id = str(time.time())
-    
     try:
+        
+        daemon_id = str(time.time())
         ecs = boto3.client('ecs')
-        run_task_res = ecs.run_task(
-                cluster=cluster,
-                taskDefinition=taskDefinition,
-                launchType=launchType,
-                networkConfiguration=networkConfiguration,
-                tags=[ { 'key' : 'DCP_DAEMON_ID' , 'value' : daemon_id } ]
-            )
+        cluster = 'daemon-daemon-DaemonEcsCluster-OBWV6JAR18NG'
+        task_definition = event.get('taskDefinition')
         
+        run_daemon_res = run_daemon(ecs, daemon_id, cluster, task_definition)
         
+        return {
+            'daemon_id' : daemon_id,
+            'taskArn' : run_daemon_res.get('taskArn')
+        }
+
     except ClientError as e:
         traceback.print_exc()
         raise e
@@ -45,11 +35,60 @@ def lambda_handler(event, context):
         traceback.print_exc()
         raise e
     
+
+def run_daemon(ecs, daemon_id, cluster, task_definition):
+    launch_type='FARGATE'
+    network_configuration={
+        'awsvpcConfiguration' : {
+            'subnets' : ['subnet-03ea9b862022ca374','subnet-03ebf3acc84659532'],
+            # 'securityGroups' : [ '' ],
+            'assignPublicIp' : 'ENABLED'
+        },
+    }
+    
+    container_overrides = create_container_overrides( ecs, task_definition, daemon_id )
+    overrides = {
+        'containerOverrides' : container_overrides
+    }
+
+    run_task_res = ecs.run_task(
+            cluster=cluster,
+            taskDefinition=task_definition,
+            launchType=launch_type,
+            networkConfiguration=network_configuration,
+            overrides=overrides,
+            tags=[ { 'key' : 'DCP_DAEMON_ID' , 'value' : daemon_id } ]
+        )
+    
     print(run_task_res)
     
-    ret = {
+    return {
         'taskArn' : run_task_res.get('tasks')[0].get('taskArn'),
         'daemon_id' : daemon_id
     }
+
+
+def create_container_overrides( ecs, task_definition, daemon_id ):
     
-    return ret
+    res = ecs.describe_task_definition( taskDefinition=task_definition )
+    
+    container_definitions = res.get('taskDefinition').get('containerDefinitions')
+    
+    container_overrides = []
+    
+    for container_definition in container_definitions:
+        name = container_definition.get('name')
+        environment = container_definition.get('environment')
+        environment.append({
+            'name': 'DCP_DAEMON_ID',
+            'value': daemon_id
+        })
+        
+        container_overrides.append({
+            'name': name,
+            'environment': environment
+        })
+
+    return container_overrides
+    
+    
